d52f9dac3d721212611eb4c3647902c2ba08114e
Jim Seconde <jim.seconde@googlemail.com>
2018-08-22 00:06:43 +0100

    starting to wireframe dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,48 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
+    <SuiContainer>
+      <SuiSidebar
+        visible=true
+        width='wide'>
+          <a class="item">
+            <i class="home icon"></i>
+            Home
+          </a>
+          <a class="item">
+            <i class="plus square outline icon"></i>
+            Add Item
+          </a>
+          <a class="item">
+            <i class="cogs icon"></i>
+            Settings
+          </a>
+          <a class="item">
+            <i class="sign out alternate icon"></i>
+            Logout
+          </a>
+      </SuiSidebar>
+      <h1 is="sui-header">My Wardrobe</h1>
+      <sui-item-group divided>
+        <sui-item>
+          <sui-item-image size="tiny" src="/static/images/placeholder.png" />
+          <sui-item-content vertical-align="middle">
+            <sui-item-header href="javascript:void(0)">T-Shirt</sui-item-header>
+          </sui-item-content>
+        </sui-item>
+        <sui-item>
+          <sui-item-image size="tiny" src="/static/images/placeholder.png" />
+          <sui-item-content vertical-align="middle">
+            <sui-item-header href="javascript:void(0)">Jumper</sui-item-header>
+          </sui-item-content>
+        </sui-item>
+        <sui-item>
+          <sui-item-image size="tiny" src="/static/images/placeholder.png" />
+          <sui-item-content vertical-align="middle">
+            <sui-item-header href="javascript:void(0)">Jeans</sui-item-header>
+          </sui-item-content>
+        </sui-item>
+      </sui-item-group>
+    </SuiContainer>
   </div>
 </template>
 
@@ -9,7 +51,7 @@ export default {
   name: 'Dashboard',
   data () {
     return {
-      msg: '[debug] Dashboard Route'
+      msg: 'My Wardrobe'
     }
   }
 }
