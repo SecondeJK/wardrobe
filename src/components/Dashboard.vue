edf4cec73313e5b4254532a00feca34770858686
Jim Seconde <jim.seconde@googlemail.com>
2018-08-21 22:48:51 +0100

    init semantic vue

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  data () {
+    return {
+      msg: '[debug] Dashboard Route'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
