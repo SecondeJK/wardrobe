33f94fc98e83039a98ed0c4b795e39d8dbf450c7
Jim Seconde <jim.seconde@googlemail.com>
2018-08-29 23:44:06 +0100

    changed to sui menu, still not fixed content pane

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <SuiContainer>
-      <SuiSidebar
-        visible=true
-        width='wide'>
+      <SuiMenu
+      vertical=true
+      fixed='left'>
           <a class="item">
             <i class="home icon"></i>
             Home
@@ -20,29 +20,29 @@
             <i class="sign out alternate icon"></i>
             Logout
           </a>
-      </SuiSidebar>
-      <h1 is="sui-header">My Wardrobe</h1>
-      <sui-item-group divided>
-        <sui-item>
-          <sui-item-image size="tiny" src="/static/images/placeholder.png" />
-          <sui-item-content vertical-align="middle">
-            <sui-item-header href="javascript:void(0)">T-Shirt</sui-item-header>
-          </sui-item-content>
-        </sui-item>
-        <sui-item>
-          <sui-item-image size="tiny" src="/static/images/placeholder.png" />
-          <sui-item-content vertical-align="middle">
-            <sui-item-header href="javascript:void(0)">Jumper</sui-item-header>
-          </sui-item-content>
-        </sui-item>
-        <sui-item>
-          <sui-item-image size="tiny" src="/static/images/placeholder.png" />
-          <sui-item-content vertical-align="middle">
-            <sui-item-header href="javascript:void(0)">Jeans</sui-item-header>
-          </sui-item-content>
-        </sui-item>
-      </sui-item-group>
-    </SuiContainer>
+      </SuiMenu>
+        <h1 is="sui-header">{{ msg }}</h1>
+        <sui-item-group divided>
+          <sui-item>
+            <sui-item-image size="tiny" src="/static/images/placeholder.png" />
+            <sui-item-content vertical-align="middle">
+              <sui-item-header href="javascript:void(0)">T-Shirt</sui-item-header>
+            </sui-item-content>
+          </sui-item>
+          <sui-item>
+            <sui-item-image size="tiny" src="/static/images/placeholder.png" />
+            <sui-item-content vertical-align="middle">
+              <sui-item-header href="javascript:void(0)">Jumper</sui-item-header>
+            </sui-item-content>
+          </sui-item>
+          <sui-item>
+            <sui-item-image size="tiny" src="/static/images/placeholder.png" />
+            <sui-item-content vertical-align="middle">
+              <sui-item-header href="javascript:void(0)">Jeans</sui-item-header>
+            </sui-item-content>
+          </sui-item>
+        </sui-item-group>
+      </SuiContainer>
   </div>
 </template>
 
@@ -51,7 +51,7 @@ export default {
   name: 'Dashboard',
   data () {
     return {
-      msg: 'My Wardrobe'
+      msg: 'Wardrobe Name'
     }
   }
 }
