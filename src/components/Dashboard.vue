68a0bcdb0cc5e38abaeb12eec4b6572c3bce452a
Jim Seconde <jim.seconde@googlemail.com>
2018-09-03 23:37:47 +0100

    added example of grid because nothing is explained and its behaviour is shit

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <SuiContainer>
-      <SuiMenu
-      vertical=true
+      <!-- <SuiMenu
+      vertical=true,
       fixed='left'>
           <a class="item">
             <i class="home icon"></i>
@@ -20,29 +20,38 @@
             <i class="sign out alternate icon"></i>
             Logout
           </a>
-      </SuiMenu>
-        <h1 is="sui-header">{{ msg }}</h1>
-        <sui-item-group divided>
-          <sui-item>
-            <sui-item-image size="tiny" src="/static/images/placeholder.png" />
-            <sui-item-content vertical-align="middle">
-              <sui-item-header href="javascript:void(0)">T-Shirt</sui-item-header>
-            </sui-item-content>
-          </sui-item>
-          <sui-item>
-            <sui-item-image size="tiny" src="/static/images/placeholder.png" />
-            <sui-item-content vertical-align="middle">
-              <sui-item-header href="javascript:void(0)">Jumper</sui-item-header>
-            </sui-item-content>
-          </sui-item>
-          <sui-item>
-            <sui-item-image size="tiny" src="/static/images/placeholder.png" />
-            <sui-item-content vertical-align="middle">
-              <sui-item-header href="javascript:void(0)">Jeans</sui-item-header>
-            </sui-item-content>
-          </sui-item>
-        </sui-item-group>
-      </SuiContainer>
+      </SuiMenu> -->
+
+      <!-- <h1 is="sui-header">{{ msg }}</h1>
+      <sui-item-group divided>
+        <sui-item>
+          <sui-item-image size="tiny" src="/static/images/placeholder.png" />
+          <sui-item-content vertical-align="middle">
+            <sui-item-header href="javascript:void(0)">T-Shirt</sui-item-header>
+          </sui-item-content>
+        </sui-item>
+        <sui-item>
+          <sui-item-image size="tiny" src="/static/images/placeholder.png" />
+          <sui-item-content vertical-align="middle">
+            <sui-item-header href="javascript:void(0)">Jumper</sui-item-header>
+          </sui-item-content>
+        </sui-item>
+        <sui-item>
+          <sui-item-image size="tiny" src="/static/images/placeholder.png" />
+          <sui-item-content vertical-align="middle">
+            <sui-item-header href="javascript:void(0)">Jeans</sui-item-header>
+          </sui-item-content>
+        </sui-item>
+      </sui-item-group> -->
+
+      <div class="ui grid">
+        <div class="ui four wide column" style="background-color: blue; height:100px">
+        </div>
+        <div class="ui twelve wide column" style="background-color: red">
+        </div>
+      </div>
+
+    </SuiContainer>
   </div>
 </template>
 
